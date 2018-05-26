fc9d84dcf8c6a04359a0cc4eb01b35d47c977b3b
llauderesv <llauderesv@gmail.com>
2018-05-26 20:17:33 +0800

    first commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a sample SSL Configuration in AWS ElasticBeanstalk.
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo '<h1>Hello SSL Certificate</h1><br />';
+
+echo $_ENV['LETSENCRYPT_DOMAIN'];
+
+echo $_ENV['LETSENCRYPT_EMAIL'];
